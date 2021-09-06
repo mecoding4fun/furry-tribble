9231fec58c82e4cc173c5560a12df953826706d1
Tarun Sankar <53414760+mecoding4fun@users.noreply.github.com>
2021-09-06 20:06:34 +0530

    Add files via upload

---
+++ Astrobot.py
@@ -0,0 +1,19 @@
+import telebot
+import datetime
+# from telebot import types
+bot = telebot.TeleBot("1640805626:AAF9_CSGSagtrHWQmdxUgtzt4oTrafEsQn4")
+
+@bot.message_handler(commands=['promos'])
+def promote(message):
+    id = message.text.split()
+    bot.promote_chat_member(message.chat.id,id[1],can_change_info=True,can_delete_messages=True,can_invite_users=True,can_restrict_members=True,can_pin_messages=True,can_promote_members=True)
+
+@bot.message_handler(commands=['my_id'])
+def send_id(message):
+    bot.send_message(message.chat.id,"Your id: "+str(message.from_user.id))
+
+@bot.message_handler(commands=['start'])
+def starts(message):
+    bot.send_message(message.chat.id,"Hey there, Check the channel @Astronomydeck")
+
+bot.polling()
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python3 Astrobot.py
+worker: python3 Astrobot.py
