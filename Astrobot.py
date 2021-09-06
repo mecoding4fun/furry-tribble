74feadf3b14901ddb2f3c0ec931d859f9ff373e9
Tarun Sankar <53414760+mecoding4fun@users.noreply.github.com>
2021-09-06 20:40:52 +0530

    Update Astrobot.py

--- Astrobot.py
+++ Astrobot.py
@@ -3,6 +3,7 @@ import datetime
 # from telebot import types
 bot = telebot.TeleBot("1879281515:AAEyNmgzQREkGsxR2oI8CW1twXiGCrbWPYA")
 
-bot.send_message('@Darknessoft','Hello)   
+bot.send_message('@Darknessoft','Hello)
+print('Bot started i guess vro')
 bot.set_webhook(url="http://example.com", certificate=open('mycert.pem'))
 bot.polling()
