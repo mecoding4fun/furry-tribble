b1df655935dc4115026fe451f930e9aa095b10d2
Tarun Sankar <53414760+mecoding4fun@users.noreply.github.com>
2021-09-06 20:38:14 +0530

    Update Astrobot.py

--- Astrobot.py
+++ Astrobot.py
@@ -3,17 +3,6 @@ import datetime
 # from telebot import types
 bot = telebot.TeleBot("1879281515:AAEyNmgzQREkGsxR2oI8CW1twXiGCrbWPYA")
 
-@bot.message_handler(commands=['promos'])
-def promote(message):
-    id = message.text.split()
-    bot.promote_chat_member(message.chat.id,id[1],can_change_info=True,can_delete_messages=True,can_invite_users=True,can_restrict_members=True,can_pin_messages=True,can_promote_members=True)
-
-@bot.message_handler(commands=['my_id'])
-def send_id(message):
-    bot.send_message(message.chat.id,"Your id: "+str(message.from_user.id))
-
-@bot.message_handler(commands=['start'])
-def starts(message):
-    bot.send_message(message.chat.id,"Hey there, Check the channel @Astronomydeck")
-
+bot.send_message('@Darknessoft','Hello)   
+bot.set_webhook(url="http://example.com", certificate=open('mycert.pem'))
 bot.polling()
