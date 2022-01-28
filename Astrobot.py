7c1f3708aef90bbbb7d586845a053924317c22a1
Tarun Sankar <53414760+mecoding4fun@users.noreply.github.com>
2022-01-28 16:33:19 +0530

    Delete Astrobot.py

--- Astrobot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-import datetime
-# from telebot import types
-bot = telebot.TeleBot("1879281515:AAEyNmgzQREkGsxR2oI8CW1twXiGCrbWPYA")
-
-bot.send_message('@Darknessoft','Hello)
-print('Bot started i guess vro')
-bot.set_webhook(url="http://example.com", certificate=open('mycert.pem'))
-bot.polling()
